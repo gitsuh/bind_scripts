d7cfcd3219a4d0d3d36594960ece83008962b1b9
good-paste <good-paste@users.noreply.github.com>
2017-12-16 04:40:02 -0500

    Update add_a_ptr_record.sh

--- add_a_ptr_record.sh
+++ add_a_ptr_record.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [[ $# -eq 0 ]] ; then
         echo 'Must provide arguments.'
-        echo './newdnsrecord.sh <forwardzonefile> <reversezonefile> <ipaddress> <FQDN>'
+        echo './add_a_ptr_record.sh <forwardzonefile> <reversezonefile> <ipaddress> <FQDN>'
         exit 1
 fi
 reversezonefile=$1
